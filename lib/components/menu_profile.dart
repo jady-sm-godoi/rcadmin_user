bb2272ce85216402ff74e89830dd05fe7d4ab847
Jady Sobjak de Mello Godoi <jady.s.m@gmail.com>
2022-03-17 17:20:33 -0300

    first commit

---
+++ lib/components/menu_profile.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+class MenuProfile extends StatefulWidget {
+  const MenuProfile({Key? key}) : super(key: key);
+
+  @override
+  _MenuProfileState createState() => _MenuProfileState();
+}
+
+class _MenuProfileState extends State<MenuProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+        icon: Icon(Icons.person),
+        itemBuilder: (context) => [
+              PopupMenuItem(
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.card_membership,
+                      color: Colors.black,
+                    ),
+                    Spacer(),
+                    Text('Profile'),
+                  ],
+                ),
+                onTap: () {},
+                value: 1,
+              ),
+              PopupMenuItem(
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.password,
+                      color: Colors.black,
+                    ),
+                    Spacer(),
+                    Text('Senha'),
+                  ],
+                ),
+                onTap: () {},
+                value: 1,
+              ),
+              PopupMenuItem(
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.dark_mode,
+                      color: Colors.black,
+                    ),
+                    Spacer(),
+                    Text('Dark/Light'),
+                  ],
+                ),
+                onTap: () {},
+                value: 1,
+              ),
+              PopupMenuItem(
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.exit_to_app,
+                      color: Colors.black,
+                    ),
+                    Spacer(),
+                    Text('Sair'),
+                  ],
+                ),
+                onTap: () {},
+                value: 1,
+              ),
+            ]);
+  }
+}
