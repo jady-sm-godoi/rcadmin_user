a26da76e705a8d08648e4060348111882634f6e7
Jady Sobjak de Mello Godoi <jady.s.m@gmail.com>
2022-03-29 20:58:43 -0300

    refactorin options of image in profile card

--- lib/main.dart
+++ lib/main.dart
@@ -28,26 +28,49 @@ class UserPage extends StatelessWidget {
           backgroundColor: Colors.blueGrey,
         ),
         drawer: DrawerMenuProfile(),
-        body: Container(
-          padding: EdgeInsets.all(10),
-          child: Row(children: [
-            Container(
-              height: 100,
-              width: 100,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                      'https://cdn.pixabay.com/photo/2018/01/15/07/52/woman-3083401_960_720.jpg'),
-                ),
-              ),
-            ),
-            Container(
-              height: 100,
-              // color: Colors.grey,
-              padding: EdgeInsets.only(left: 20),
+        body:
 
+            // const Card(
+            //   margin: EdgeInsets.all(20),
+            //   child: Padding(
+            //     padding: EdgeInsets.all(8.0),
+            //     child: ListTile(
+            //       leading: CircleAvatar(
+            //         radius: 30.0,
+            //         backgroundImage: NetworkImage(
+            //             'https://cdn.pixabay.com/photo/2018/01/15/07/52/woman-3083401_960_720.jpg'),
+            //       ),
+            //       title: Text('Florinda Spring'),
+            //       subtitle: Text('Aquarius (BR)\n4th. Aspect em 06/11/2016'),
+            //       isThreeLine: true,
+            //       trailing: Text('986'),
+            //     ),
+            //   ),
+            // )
+
+            Container(
+          padding: EdgeInsets.all(25),
+          child: Row(children: [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: NetworkImage(
+                  'https://cdn.pixabay.com/photo/2018/01/15/07/52/woman-3083401_960_720.jpg'),
+            ),
+            // Container(
+            //   height: 100,
+            //   width: 100,
+            //   decoration: const BoxDecoration(
+            //     shape: BoxShape.circle,
+            //     image: DecorationImage(
+            //       fit: BoxFit.cover,
+            //       image: NetworkImage(
+            //           'https://cdn.pixabay.com/photo/2018/01/15/07/52/woman-3083401_960_720.jpg'),
+            //     ),
+            //   ),
+            // ),
+            Container(
+              height: 100,
+              padding: const EdgeInsets.only(left: 20),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -56,16 +79,15 @@ class UserPage extends StatelessWidget {
                     mainAxisSize: MainAxisSize.max,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: const [
-                      Text('Aquarius'),
+                      Text('Aquarius '),
                       Text('960'),
                     ],
                   ),
-                  // Spacer(),
                   Text(
                     'Florinda Spring',
                     style: Theme.of(context).textTheme.headline6,
                   ),
-                  Text('florinda_spring@email.com')
+                  const Text('florinda_spring@email.com')
                 ],
               ),
             )
